ad6fc3df870066a3992e9a02269962ead62ad8d5
WindxFire <aeronredcelajes@gmail.com>
2023-10-16 16:13:33 +0800

    adapterPattern code here

--- adapterPattern/ElectronicDeviceApp.java
+++ adapterPattern/ElectronicDeviceApp.java
@@ -1,4 +1,19 @@
 package adapterPattern;
 
 public class ElectronicDeviceApp {
+
+    public static void main(String[] args){
+        Laptop laptop = new Laptop();
+        Refrigerator refrigerator = new Refrigerator();
+        SmartphoneCharger smartphoneCharger = new SmartphoneCharger();
+
+        PowerOutlet laptopOutlet = new LaptopAdapter(laptop);
+        PowerOutlet refrigeratorOutlet = new RefrigeratorAdapter(refrigerator);
+        PowerOutlet smartphoneChargerOutlet = new SmartphoneAdapter(smartphoneCharger);
+
+        // This is where the devices are plug into the power outlets
+        System.out.println(laptopOutlet.plugIn());
+        System.out.println(refrigeratorOutlet.plugIn());
+        System.out.println(smartphoneChargerOutlet.plugIn());
+    }
 }
